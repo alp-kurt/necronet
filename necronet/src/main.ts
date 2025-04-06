cf96dc0cff57396ad00798d965573d69986d70e0
Alp Kurt <krtalp@gmail.com>
2025-04-06 23:30:24 +0200

    test: add random joke and fact displayers

--- necronet/src/main.ts
+++ necronet/src/main.ts
@@ -2,6 +2,8 @@ import { Application, Container } from 'pixi.js';
 import { Resizer } from './resizer';
 import { createUserDisplay } from './ui/userDisplay';
 import { createFetchUserButton } from './ui/fetchUserButton';
+import { createRandomJokeText } from './ui/randomJoke';
+import { createRandomFactText } from './ui/randomFact';
 
 const GAME_WIDTH = 1080;
 const GAME_HEIGHT = 1080;
@@ -25,6 +27,18 @@ new Resizer(app, gameContainer, GAME_WIDTH, GAME_HEIGHT);
 
 // Display: Avatar + NameText
 const { nameText, avatarSpriteRef } = createUserDisplay(gameContainer, GAME_WIDTH);
+
+const jokeText = await createRandomJokeText();
+jokeText.x = GAME_WIDTH / 2;
+jokeText.y = 600;
+gameContainer.addChild(jokeText);
+
+const factText = await createRandomFactText();
+factText.x = GAME_WIDTH / 2;
+factText.y = 680; 
+
+gameContainer.addChild(factText);
+
 
 // Button
 const fetchButton = createFetchUserButton({
